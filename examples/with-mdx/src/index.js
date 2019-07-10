3074d31731b32e3b95d997fc5399fcf9ba9523ff
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2019-07-10 20:11:43 +0200

    feat(with-mdx): Add frontmatter

--- examples/with-mdx/src/index.js
+++ examples/with-mdx/src/index.js
@@ -1,10 +1,12 @@
 const { Builder, source, write, clean } = require("@tatao/core");
+const { frontmatter } = require("@tatao/plugin-frontmatter");
 const { mdx } = require("@tatao/plugin-mdx");
 const { react } = require("@tatao/plugin-react");
 
 new Builder({ locales: ["de", "en"] })
   .use(clean(["./public"]))
   .use(source("./src/content/", ["/**"]))
+  .use(frontmatter())
   .use(mdx())
   .use(react())
   .use(write("./public"))
